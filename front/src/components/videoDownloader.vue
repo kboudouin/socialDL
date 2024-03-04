f42fe3ddd7af56d9fe60b1d44205cbd2bf8b9462
kieran <kieran.boudouin@epitech.eu>
2024-03-04 23:23:00 +0100

    Video Layout imporovement

--- front/src/components/videoDownloader.vue
+++ front/src/components/videoDownloader.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex flex-col items-center justify-center">
-    <h1 class="font-bold text-4xl lg:mx-12" v-if="data.title  != ''">{{ data.title }}</h1>
-    <p class="text-3xl" v-if="data.view_count  != ''">{{ formatNumber(data.view_count) }} views</p>
-    <h2 v-if="data.uploader  != ''">Uploaded by: {{ data.uploader }}</h2>
+    <h1 class="font-bold text-lg lg:text-4xl lg:mx-12" v-if="data.title  != ''">{{ data.title }}</h1>
+    <p class="text-xl lg:text-3xl" v-if="data.view_count  != ''">{{ formatNumber(data.view_count) }} views</p>
+    <h2 class="text-lg lg:text-2xl" v-if="data.uploader  != ''">Uploaded by: {{ data.uploader }}</h2>
     <div class="my-2">
         <button @click="downloadVideoFile" class="btn btn-xl btn-primary">Download</button>
     </div>
-    <div class="lg:w-1/2 card shadow-xl">
-         <video class="" v-if="data.filepath  != '' && data.ext == 'mp4' " controls :src="data.filepath"></video>
+    <div class="card shadow-xl">
+         <video class="max-h-96" v-if="data.filepath  != '' && data.ext == 'mp4' " controls :src="data.filepath"></video>
     </div>
     <audio v-if="data.filepath  != '' && data.ext != 'mp4'" controls :src="data.filepath"></audio>
-    <p v-if="data.like_count  != '' && data.like_count == '-1'  ">Likes: {{ formatNumber(data.like_count) }}</p>
-    <p v-if="data.dislike_count  != ''">Dislikes: {{ formatNumber(data.dislike_count) }}</p>
-    <p v-if="data.channel_id  != ''">Channel ID: {{ data.channel_id }}</p>
-    <p v-if="data.uploader_id  != ''">Uploader ID: {{ data.uploader_id }}</p>
-
+    <div  class="flex flex-col items-center justify-center mx-16 lg:m-6">
+      <p class="text-lg lg:text-md" v-if="data.like_count  != '' && data.like_count == '-1'  ">Likes: {{ formatNumber(data.like_count) }}</p>
+      <p class="text-lg lg:text-md" v-if="data.dislike_count  != ''">Dislikes: {{ formatNumber(data.dislike_count) }}</p>
+      <p class="text-sm lg:text-md" v-if="data.channel_id  != ''">Channel ID: {{ data.channel_id }}</p>
+      <p class="text-sm lg:text-md" v-if="data.uploader_id  != ''">Uploader ID: {{ data.uploader_id }}</p>
+    </div>
   </div>
 </template>
 
